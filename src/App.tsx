8a8591b07868c2b2d2d5b2f286e932784ad69bf4
quangoku <muakaka5@gmail.com>
2025-05-01 23:59:45 +0700

    update css bg

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
   }, [count]);
   return (
     <>
-      <div className="bg-blue-300 h-auto text-center">
+      <div className="bg-blue-300 h-screen text-center">
         <h1 className="text-center text-5xl p-5">POKEMON LIST</h1>
         <input
           type="text"
@@ -42,16 +42,19 @@ function App() {
               <Pokemon key={pokemon.name} url={pokemon.url}></Pokemon>
             ))}
         </div>
-        <button
-          className="
+        <div className="bg-blue-300">
+          {" "}
+          <button
+            className="
         cursor-pointer p-5
          bg-amber-100"
-          onClick={() => {
-            setCount(count + 5);
-          }}
-        >
-          LOADMORE
-        </button>
+            onClick={() => {
+              setCount(count + 5);
+            }}
+          >
+            LOADMORE
+          </button>
+        </div>
       </div>
     </>
   );
